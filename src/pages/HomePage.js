838e60678b7e43d459027a41def9a7298c5f2df9
adilabbasi-786 <m.adilabbasi786@gmail.com>
2023-07-04 17:32:32 +0500

    implement about

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Carousel from '../components/Carousel/Carousel'
 import Service from '../components/Services/Service'
+import About from '../components/About/About'
 
 const HomePage = () => {
   return (
     <>
      <Carousel/>
      <Service/> 
+     <About/>
     </>
   )
 }
