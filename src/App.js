c95e9d9b35ec7801d08a7280ca6ef4a733285ae7
adilabbasi-786 <m.adilabbasi786@gmail.com>
2023-07-04 16:54:13 +0500

    implemented pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
 import Carousel from "./components/Carousel/Carousel";
-import TopNavBar from "./components/TopNavBar/TopNavBar";
-import SecondNavBar from "./components/secondNavBar/SecondNavBar";
-
+import TopNavBar from "./components/Home/TopNavBar/TopNavBar";
+import SecondNavBar from "./components/Home/SecondNavBar.js/SecondNavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import NotFoundPage from "./pages/NotFoundPage";
 function App() {
   return (
     <>
+    <BrowserRouter>
       <TopNavBar />
       <SecondNavBar />
-      <Carousel />
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/about" element={<AboutPage/>}/>
+      <Route path="/contact" element={<ContactPage/>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
+
+    </Routes>
+      {/* <Carousel />  */}
+    </BrowserRouter>
     </>
   );
 }
