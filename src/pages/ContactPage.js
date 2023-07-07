05bf77e355840ef87d9703cda800b9b941dd038f
adilabbasi-786 <m.adilabbasi786@gmail.com>
2023-07-07 12:08:25 +0500

    contact us from back end

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Crousel1 from "../img/carousel-bg-1.jpg";
 
 const ContactPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const handleClick = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        data: { email: email, name: name, subject: subject, message: message },
+      }),
+    };
+    const response = await fetch(
+      "http://localhost:1337/api/contacts",
+      requestOptions
+    );
+
+    const data = await response.json();
+    setName("");
+    setEmail("");
+    setSubject("");
+    setMessage("");
+
+    let reqData = {
+      datas: {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message,
+      },
+    };
+  };
   return (
     <>
       <div
@@ -85,64 +117,69 @@ const ContactPage = () => {
             <div class="col-md-6">
               <div class="wow fadeInUp" data-wow-delay="0.2s">
                 <p class="mb-4">
-                  The contact form is currently inactive. Get a functional and
-                  working contact form with Ajax & PHP in a few minutes. Just
-                  copy and paste the files, add a little code and you're done.{" "}
-                  <a href="https://htmlcodex.com/contact-form">Download Now</a>.
+                  please provide us the problems that you faced during our
+                  Services
                 </p>
-                <form>
-                  <div class="row g-3">
-                    <div class="col-md-6">
-                      <div class="form-floating">
-                        <input
-                          type="text"
-                          class="form-control"
-                          id="name"
-                          placeholder="Your Name"
-                        />
-                        <label for="name">Your Name</label>
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-floating">
-                        <input
-                          type="email"
-                          class="form-control"
-                          id="email"
-                          placeholder="Your Email"
-                        />
-                        <label for="email">Your Email</label>
-                      </div>
-                    </div>
-                    <div class="col-12">
-                      <div class="form-floating">
-                        <input
-                          type="text"
-                          class="form-control"
-                          id="subject"
-                          placeholder="Subject"
-                        />
-                        <label for="subject">Subject</label>
-                      </div>
-                    </div>
-                    <div class="col-12">
-                      <div class="form-floating">
-                        <textarea
-                          class="form-control"
-                          placeholder="Leave a message here"
-                          id="message"
-                          style={{ height: "100px" }}
-                        ></textarea>
-                        <label for="message">Message</label>
-                      </div>
-                    </div>
-                    <div class="col-12">
-                      <button class="btn btn-primary w-100 py-3" type="submit">
-                        Send Message
-                      </button>
+
+                <div class="row g-3">
+                  <div class="col-md-6">
+                    <div class="form-floating">
+                      <input
+                        onChange={(e) => setName(e.target.value)}
+                        type="text"
+                        class="form-control"
+                        id="name"
+                        placeholder="Your Name"
+                      />
+                      <label for="name">Your Name</label>
                     </div>
                   </div>
-                </form>
+                  <div class="col-md-6">
+                    <div class="form-floating">
+                      <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        class="form-control"
+                        id="email"
+                        placeholder="Your Email"
+                      />
+                      <label for="email">Your Email</label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <div class="form-floating">
+                      <input
+                        onChange={(e) => setSubject(e.target.value)}
+                        type="text"
+                        class="form-control"
+                        id="subject"
+                        placeholder="Subject"
+                      />
+                      <label for="subject">Subject</label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <div class="form-floating">
+                      <textarea
+                        onChange={(e) => setMessage(e.target.value)}
+                        class="form-control"
+                        placeholder="Leave a message here"
+                        id="message"
+                        style={{ height: "100px" }}
+                      ></textarea>
+                      <label for="message">Message</label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <button
+                      onClick={handleClick}
+                      class="btn btn-primary w-100 py-3"
+                      type="submit"
+                    >
+                      Send Message
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
