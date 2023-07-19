6c0809d15e5dc3b7a7e3059178d985a28efc03f8
adilabbasi-786 <m.adilabbasi786@gmail.com>
2023-07-19 21:13:12 +0500

    updated url

--- src/context/WebContext.js
+++ src/context/WebContext.js
@@ -1,5 +1,6 @@
 import React, { createContext, useEffect, useState } from "react";
 import Loader from "../components/loader/Loader";
+import { URL } from "../utills";
 export const WebContext = createContext();
 function Webinfo(props) {
   const [loading, setLoading] = useState(true);
@@ -8,7 +9,7 @@ function Webinfo(props) {
   useEffect(() => {
     const getData = async () => {
       setLoading(true);
-      let request = await fetch("http://localhost:1337/api/web-info");
+      let request = await fetch(`${URL}/api/web-info`);
       let res = await request.json();
       setWebinfoData(res);
 
---
+++ src/utills.js
@@ -0,0 +1,4 @@
+export const URL =
+  process.env === "development"
+    ? "http://localhost:1337"
+    : "https://carbackend-0427c9649cf9.herokuapp.com";
